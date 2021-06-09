7e2acbc05503e7f7fbae2421813b5612fb14c0c1
unknown <rdg.abrantes@gmail.com>
2021-06-08 23:09:10 -0300

    configurado a conexao com o SQL Server via jpa, no application properties. Ja foi testado e feito o build uma vez.

---
+++ src/main/java/controller/JyaApplication.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(JyaApplication.class, args);
+    }
+
+}
